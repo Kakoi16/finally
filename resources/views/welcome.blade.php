ea3c23e12b40bfae516b6abfb3a564083c4aa9a0
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-16 08:24:23 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
     </style>
 </head>
 <body class="min-h-screen text-white">
-    <!-- Navigation -->
+    <!-- Navigations -->
     <nav class="glass-nav fixed w-full z-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16 items-center">
