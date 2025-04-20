2a49533390133ff74ff7f17db8300fe4a6ea71b9
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-20 22:47:59 +0700

    navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/components/navbar.blade.php -->
+<nav class="bg-white shadow-md p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="/" class="text-xl font-bold text-gray-800">📁 File Archive</a>
+        <div class="space-x-4">
+            <a href="/" class="text-gray-600 hover:text-blue-600">Home</a>
+            <a href="#" class="text-gray-600 hover:text-blue-600">Templates</a>
+            <a href="#" class="text-gray-600 hover:text-blue-600">Pengajuan Surat</a>
+        </div>
+    </div>
+</nav>
