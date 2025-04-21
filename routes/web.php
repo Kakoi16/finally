1f8fd9c734057c551b42144213cf0a2c730ce82a
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-21 22:51:36 +0700

    asa

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 
-    // Register
+    // Register (UI & API AJAX)
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'registerKaryawan']);
+    Route::post('/register', [AuthController::class, 'registerKaryawan'])->name('register.karyawan');
 
     // Email Verification
     Route::get('/verify/{token}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
@@ -20,7 +20,6 @@ Route::middleware('guest')->group(function () {
     // Forgot & Reset Password
     Route::get('/forgot-password', [AuthController::class, 'showForgot'])->name('password.request');
     Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
-
     Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
     Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
 });
