f6fd3e8d630c1c1b85fc947cbda5c7eb7122d42b
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-21 22:34:01 +0700

    as

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -92,7 +92,7 @@ class AuthController extends Controller
             return back()->withErrors(['error' => 'Gagal mengirim email: ' . $e->getMessage()]);
         }
     
-        return redirect()->route('archive')->with('success', 'Link verifikasi telah dikirim ke email Anda.');
+        return back()->with('success', 'Link verifikasi telah dikirim ke email Anda.');
     }
     
 
