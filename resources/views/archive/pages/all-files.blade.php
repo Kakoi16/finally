4649fd09f836c10d528898bd35ad6e53be86eebd
yuliaalaila <if23.yuliahasanah@mhs.ubpkarawang.ac.id>
2025-04-26 15:15:01 +0700

    aa

--- resources/views/archive/pages/all-files.blade.php
+++ resources/views/archive/pages/all-files.blade.php
@@ -12,7 +12,7 @@
                     <label for="file" class="block text-sm font-medium text-gray-700">Pilih file untuk diupload</label>
                     <input type="file" name="file" id="file" class="mt-2 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none">
                 </div>
-                <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Upload
                 </button>
             </form>
@@ -27,7 +27,7 @@
                     <label for="folder_name" class="block text-sm font-medium text-gray-700">Nama Folder</label>
                     <input type="text" name="folder_name" id="folder_name" class="mt-2 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg focus:outline-none p-2">
                 </div>
-                <button type="submit" class="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700">
+                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                     Buat Folder
                 </button>
             </form>
