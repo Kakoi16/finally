4f2346a30099029256f3f5a14fe4a8dfd32e7ab0
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-26 12:37:55 +0700

    as

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GoogleController;
-use App\Http\Controllers\ArchiveController;
 
 Route::middleware('guest')->group(function () {
     // Login
@@ -39,7 +38,7 @@ Route::get('/', function () {
     return view('layouts.home');
 });
 
-Route::get('/archive', [ArchiveController::class, 'index'])->name('archive')->middleware('admin.only');
+Route::get('/archive', [DashboardController::class, 'index'])->name('archive')->middleware('admin.only');
 
 
 // Google login
