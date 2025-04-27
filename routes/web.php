bf7fb8627d1d41161d01afc8c16d4d714b968d84
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-27 15:22:48 +0700

    fiks 3

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,8 @@ Route::post('/folders/create', [FileController::class, 'createFolder'])->name('f
 
 // routes/folders
 // routes/web.php
-Route::get('/folders/{folderName}', [FolderController::class, 'show']);
+Route::get('/folders/{folderName}', [FolderController::class, 'show'])->name('folders.open');
+
 Route::post('/folders', [FolderController::class, 'store'])->name('folders.create');
 
 Route::get('/files', [FileController::class, 'index'])->name('files.index');
