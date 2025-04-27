c551ee6e191ee4e598a46681ba55ac575542688d
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-27 14:04:43 +0700

    again

--- resources/views/archive/pages/all-files.blade.php
+++ resources/views/archive/pages/all-files.blade.php
@@ -1,5 +1,11 @@
 <!-- resources/views/all-files.blade.php -->
-
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archive Perusahaan</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    @stack('styles')
+</head>
 @php
 $files = $archives ?? [];
 @endphp
