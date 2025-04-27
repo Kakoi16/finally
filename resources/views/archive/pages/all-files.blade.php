614ea92d2075e89801509ca5d20c29886f87efce
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-27 14:25:57 +0700

    uang

--- resources/views/archive/pages/all-files.blade.php
+++ resources/views/archive/pages/all-files.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archive Perusahaan</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
-    <!-- resources/views/all-files.blade.php -->
+<!-- resources/views/all-files.blade.php -->
 
 @php
 $files = $archives ?? [];
@@ -211,5 +201,3 @@ $files = $archives ?? [];
         form.classList.toggle('hidden');
     }
 </script>
-</body>
-</html>
