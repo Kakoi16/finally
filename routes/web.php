aef8ef1f771fc7ed059486d02206484c4028f07d
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-04-28 15:37:10 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -52,15 +52,17 @@ Route::post('/folders/create', [FileController::class, 'createFolder'])->name('f
 // routes/web.php
 Route::middleware(['admin.only'])->group(function () {
     // Folder
-Route::post('/folders/{parentFolder}/create-subfolder', [FolderController::class, 'createSubfolder'])->name('folders.createSubfolder');
+    Route::get('/folders/{parentFolder}/{subFolder}', [FolderController::class, 'showSubfolder'])->name('folders.subfolder');
 
-// File Upload
-Route::post('/folders/{folderName}/upload', [FileController::class, 'upload'])->name('files.uploadToFolder');
+    Route::post('/folders/{parentFolder}/create-subfolder', [FolderController::class, 'createSubfolder'])->name('folders.createSubfolder');
+
+    // File Upload
+    Route::post('/folders/{folderName}/upload', [FileController::class, 'upload'])->name('files.uploadToFolder');
 
     Route::get('/folders/{folderName}', [FolderController::class, 'show'])->name('folders.open');
     Route::get('/archive/{folderPath}', [ArchiveController::class, 'showFolder'])
-    ->where('folderPath', '.*')
-    ->name('archive.showFolder');
+        ->where('folderPath', '.*')
+        ->name('archive.showFolder');
 
     Route::post('/folders', [FolderController::class, 'createFolder'])->name('folders.create');
 });
