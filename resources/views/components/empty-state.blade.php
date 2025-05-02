0c6abcc1a1820424f238eed71df930a906f41647
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-05-02 08:21:05 +0700

    empty-state

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -1,4 +1,4 @@
-<!-- Empty State -->
+<!-- Emptfy State -->
 <div class="text-center py-16 bg-gray-50 rounded-lg border-2 border-dashed border-gray-200">
     <svg class="mx-auto h-14 w-14 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
