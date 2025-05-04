f44cc7967dd6d877cc26b9f6b5e4e8b2ed324794
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-05-04 16:11:01 +0700

    asasa

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ],
     ];
 
@@ -67,11 +68,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin.only' => \App\Http\Middleware\AdminOnly::class,
-        'api' => [
-    \Illuminate\Http\Middleware\HandleCors::class,
-    \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-],
+
 
     ];
 }
