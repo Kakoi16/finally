b4515613a551965d4f5cace3b52a78180d6f0988
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-05-04 23:19:24 +0700

    all files

--- resources/views/archive/pages/all-files.blade.php
+++ resources/views/archive/pages/all-files.blade.php
@@ -18,16 +18,6 @@ foreach ($files as $file) {
 }
 @endphp
 
-@if(session('success'))
-<div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-6 rounded">
-    <div class="flex items-center">
-        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-        </svg>
-        <p>{{ session('success') }}</p>
-    </div>
-</div>
-@endif
 
 @if(session('error'))
 <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-6 rounded">
