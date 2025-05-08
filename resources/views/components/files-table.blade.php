11d40ab532cf3389036f775f000215d313a1b5f4
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-05-08 12:10:26 +0700

    Aksi

--- resources/views/components/files-table.blade.php
+++ resources/views/components/files-table.blade.php
@@ -137,16 +137,6 @@
                                 </svg>
                             </a>
                             @endif
-                            <a href="#" class="text-gray-500 hover:text-gray-700 p-2 rounded-md hover:bg-gray-50 transition-colors" title="Download">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                                </svg>
-                            </a>
-                            <button class="delete-item-btn text-gray-500 hover:text-red-600 p-2 rounded-md hover:bg-red-50 transition-colors" title="Delete">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                            </button>
                         </div>
                     </td>
                 </tr>
